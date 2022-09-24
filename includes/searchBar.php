d06fae86718f809e995b2413266ae2626943ebe9
sagar-noob <57512118+sagar-noob@users.noreply.github.com>
2022-09-24 20:51:27 +0545

    Add files via upload

---
+++ includes/searchBar.php
@@ -0,0 +1,9 @@
+<div style="display:flex; justify-content:space-between ;">
+			<h3 style="display:inline;">Package List</h3>
+			<div style='display:inline'>
+				<form action="search.php" method="GET">
+						<input type="text" name="q" class="p-4" placeholder="Search desired packages">
+						<button type="submit" class="btn btn-sm btn-primary" >Submit</button>
+				</form>
+			</div>
+			</div>
